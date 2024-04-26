576bb59881a210ab08d37d1df81fea36b8b3b319
charvotj <jakub.charvot@centrum.cz>
2024-04-27 01:32:01 +0200

    evening :(

--- Helpers/AppSettings.cs
+++ Helpers/AppSettings.cs
@@ -4,6 +4,6 @@ public class AppSettings
 {
     public string Secret { get; set; }
     public int Port { get; set; }
-    public int Hostname { get; set; }
-    public int Scheme { get; set; }
+    public string Hostname { get; set; }
+    public string Scheme { get; set; }
 }
